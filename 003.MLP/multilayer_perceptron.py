d88eb08f04c8389f2de01cbe496ea7d379f3793e
qianhua <1983561291@qq.com>
2025-12-11 23:17:09 +0800

    Study MLP(Multilayer Perceptron)

---
+++ 003.MLP/multilayer_perceptron.py
@@ -0,0 +1,176 @@
+import os
+import matplotlib
+if os.environ.get('DISPLAY', '') == '':
+    matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+from torch import nn
+from torch.utils import data
+from torchvision import transforms
+
+os.environ['HTTP_PROXY'] = 'http://127.0.0.1:7890'
+os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:7890'
+
+def get_dataloader_workers():
+    """Use 4 processes to read the data."""
+    return 4
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    """Download the Fashion-MNIST dataset and then load it into memory."""
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(
+        root="./data", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(
+        root="./data", train=False, transform=trans, download=True)
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
+                            num_workers=get_dataloader_workers()),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,
+                            num_workers=get_dataloader_workers()))
+
+batch_size = 256
+train_iter, test_iter = load_data_fashion_mnist(batch_size)
+
+num_inputs, num_outputs, num_hiddens = 784, 10, 256
+
+W1 = nn.Parameter(torch.randn(num_inputs, num_hiddens, requires_grad=True) * 0.01)
+b1 = nn.Parameter(torch.zeros(num_hiddens, requires_grad=True))
+W2 = nn.Parameter(torch.randn(num_hiddens, num_outputs, requires_grad=True) * 0.01)
+b2 = nn.Parameter(torch.zeros(num_outputs, requires_grad=True))
+
+params = [W1, b1, W2, b2]
+
+def relu(X):
+    a = torch.zeros_like(X)
+    return torch.max(X, a)
+
+def net(X):
+    X = X.reshape((-1, num_inputs))
+    H = relu(X @ W1 + b1)   # H = relu(torch.matmul(X, W1) + b1)
+    return H @ W2 + b2      # return torch.matmul(H, W2) + b2
+
+class Accumulator:
+    """For accumulating sums over `n` variables."""
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+    
+class Animator:
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None, ylim=None,
+                 xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(6, 4)):
+        if legend is None:
+            legend = []
+        self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        self.config_axes = lambda: self.set_axes(self.axes[0], xlabel, ylabel,
+                                                 xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def set_axes(self, axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+        """Set the axes for the plot: labels, scales, limits and legend."""
+        if xlabel:
+            axes.set_xlabel(xlabel)
+        if ylabel:
+            axes.set_ylabel(ylabel)
+        axes.set_xscale(xscale)
+        axes.set_yscale(yscale)
+        if xlim:
+            axes.set_xlim(xlim)
+        if ylim:
+            axes.set_ylim(ylim)
+        if legend:
+            axes.legend(legend)
+        axes.grid()
+
+    def add(self, x, y):
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i in range(n):
+            self.X[i].append(x)
+            self.Y[i].append(y[i])
+        self.axes[0].cla()
+        for i in range(n):
+            self.axes[0].plot(self.X[i], self.Y[i], self.fmts[i])
+        self.config_axes()
+        plt.pause(0.001)
+
+def accuracy(y_hat, y):
+    """Compute the number of correct predictions."""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(dim=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+def evaluate_accuracy(net, data_iter):
+    """Evaluate the accuracy of a model on the given data set."""
+    if isinstance(net, torch.nn.Module):
+        net.eval()  # Set the model to evaluation mode
+    metric = Accumulator(2)  # No. of correct predictions, no. of predictions
+    with torch.no_grad():
+        for X, y in data_iter:
+            y_hat = net(X)
+            metric.add(accuracy(y_hat, y), y.numel())
+    return metric[0] / metric[1]
+
+def train_epoch_ch3(net, train_iter, loss, updater):
+    """Train a model within one epoch (defined in Chapter 3)."""
+    # Set the model to training mode
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    # Sum of training loss, sum of training accuracy, no. of examples
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        # Compute gradients and update parameters
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()
+        else:
+            l.sum().backward()
+            updater(X.shape[0])
+        # Update training loss and accuracy
+        with torch.no_grad():
+            metric.add(float(l.sum()), float((y_hat.argmax(dim=1) == y).sum()), y.numel())
+    # Return training loss and training accuracy
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
+    """Train a model (defined in Chapter 3)."""
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs],
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    train_loss, train_acc = train_metrics
+    print(f'final train loss: {train_loss:.4f}, '
+          f'train acc: {train_acc:.3f}, '
+          f'test acc: {test_acc:.3f}')
+    
+loss = nn.CrossEntropyLoss(reduction='none')
+num_epochs, lr = 10, 0.1
+updater = torch.optim.SGD(params, lr=lr)
+
+train_ch3(net, train_iter, test_iter, loss, num_epochs, updater)
+plt.show()
