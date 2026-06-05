d17dc4a412b6b9d587743130200de6fc769ecf82
torch-5080 <1983561291@qq.com>
2026-06-05 09:57:21 +0000

    add download.sh

---
+++ CV_paper/cv01_alexnet/src/download.sh
@@ -0,0 +1,6 @@
+rm -rf ../dataset/cats_and_dogs_filtered
+mkdir ../dataset
+cd ../dataset
+# 一键下载解压
+wget https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip
+unzip cats_and_dogs_filtered.zip
