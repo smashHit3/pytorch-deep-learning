3bbc8398d31a5a006145e5923c7a273692cd5bb8
torch-5080 <1983561291@qq.com>
2026-06-06 12:48:02 +0000

    implement google net.

---
+++ CV_paper/cv02_vgg/sources/tools/__init__.py
@@ -0,0 +1,6 @@
+"""Local tools package for cv02_vgg.
+
+Provides the data processor module for cats-vs-dogs dataset loading and splitting.
+"""
+
+__all__ = ["data_processor"]
---
+++ CV_paper/cv03_googlenet/sources/tools/__init__.py
@@ -0,0 +1,6 @@
+"""Local tools package for cv03_googlenet.
+
+Provides the data processor module for Fashion-MNIST dataset loading and splitting.
+"""
+
+__all__ = ["data_processor"]
